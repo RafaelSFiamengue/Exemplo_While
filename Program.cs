860c7c71dc90ed5b163da727e854408d7e505a4a
aluno <aluno@A2727>
2023-06-05 21:30:58 -0300

    update

--- Program.cs
+++ Program.cs
@@ -13,18 +13,22 @@ namespace Course
         {
             //Variavel para o valor num
             double num;
-            //Faça
-            do
+            
+            //Imprime para o usuario o perguntando o valor entre os numeros naturais que ele quer usar
+            Console.WriteLine("Digite um numero natural: ");
+
+            //Faz a leitura do valor do usuario para a variavel
+            num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Imprime para o usuarios a quantidade de numeros pares que ele escolheu
+            Console.WriteLine("Os primeiros " + num + " numeros pares são");
+
+            //Condição de for para aparecer a contagem de 2 em 2
+            for (int i = 2; i < num * 2; i += 2)
             {
-                //Imprime para o usuario o perguntando o valor que ele quer usar entre 1 a 10
-                Console.WriteLine("Digite um numero entre 1 a 10: ");
-                //Faz a leitura do valor do usuario para a variavel
-                num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                //Imprime os números pares para o usuário
+                Console.WriteLine(i + "");
             }
-            //Condicional de While para o valor de num estar entre 1 e 10
-            while (num < 1 || num > 10);
-                //Imprime para o usuario o número que ele digitou
-                Console.WriteLine("Você digitou o numero " + num + ".");
             
             
         }
